4563e6661abb01e5571142034c4ef3c37281cdca
Luca Pasini <pasini.luca09@gmail.com>
2023-08-21 15:53:16 +0200

    Create Items table

---
+++ app/src/main/java/com/giacomosirri/myapplication/data/Item.kt
@@ -0,0 +1,29 @@
+package com.giacomosirri.myapplication.data
+
+import android.media.Image
+import android.util.Range
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Items")
+class Item {
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    val id : Int = 0
+
+    @ColumnInfo(name = "name")
+    val name : String = ""
+
+    @ColumnInfo(name = "description")
+    val description : String = ""
+
+    @ColumnInfo(name = "url")
+    val url : String = ""
+
+    @ColumnInfo(name = "image")
+    val image : Image? = null
+
+    @ColumnInfo(name = "price_range")
+    val range : Range<Double> = Range(0.0,0.0)
+}
