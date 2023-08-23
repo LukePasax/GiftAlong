095a7a9269287096d4d2784daa2c0aa0e10ffe28
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-08-23 16:43:15 +0200

    Add dependencies to build files and update AppViewModel

--- app/src/main/java/com/giacomosirri/myapplication/viewmodel/AppViewModel.kt
+++ app/src/main/java/com/giacomosirri/myapplication/viewmodel/AppViewModel.kt
@@ -3,7 +3,6 @@ package com.giacomosirri.myapplication.viewmodel
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 import dagger.hilt.android.lifecycle.HiltViewModel
-import androidx.lifecycle.viewModelScope
 import com.giacomosirri.myapplication.repository.EventRepository
 import com.giacomosirri.myapplication.repository.ItemRepository
 import com.giacomosirri.myapplication.repository.UserRepository
