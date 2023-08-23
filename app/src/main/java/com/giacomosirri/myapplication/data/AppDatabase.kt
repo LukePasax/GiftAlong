e2eaed859cefa80ffeb1f540e9c3900c9bbd1816
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-08-23 12:06:27 +0200

    Continue implementation of AppDatabase

--- app/src/main/java/com/giacomosirri/myapplication/data/AppDatabase.kt
+++ app/src/main/java/com/giacomosirri/myapplication/data/AppDatabase.kt
@@ -4,12 +4,19 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.giacomosirri.myapplication.data.dao.ItemDAO
 import com.giacomosirri.myapplication.data.entity.Event
 import com.giacomosirri.myapplication.data.entity.Item
+import com.giacomosirri.myapplication.data.entity.Relationship
 import com.giacomosirri.myapplication.data.entity.User
 
 @Database(entities = [User::class, Event::class, Item::class, Relationship::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
+    abstract fun itemDao(): ItemDAO
+    //abstract fun userDao(): UserDAO
+    //abstract fun relationshipDao(): RelationshipDAO
+    //abstract fun eventDao(): EventDAO
+
     companion object {
         @Volatile
         private var INSTANCE: AppDatabase? = null
