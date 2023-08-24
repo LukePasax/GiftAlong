bd8f30411309dbf0200d315aebca7f44299569a9
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-08-24 11:45:52 +0200

    Update colors

--- app/src/main/java/com/giacomosirri/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/giacomosirri/myapplication/ui/theme/Color.kt
@@ -1,11 +1,16 @@
 package com.giacomosirri.myapplication.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import com.giacomosirri.myapplication.R
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Primary = Color(R.color.primary)
+val Secondary = Color(0xFFD0BCFF)
+val Tertiary = Color(0xFFCCC2DC)
+val Background = Color(0xFFEFB8C8)
+val Surface = Color(0xFF6650a4)
+val OnPrimary = Color.White
+val OnSecondary = Color.White
+val OnTertiary = Color.White
+val OnBackground = Color(0xFF1C1B1F)
+val OnSurface = Color(0xFF1C1B1F)
+val Error = Color.Red
