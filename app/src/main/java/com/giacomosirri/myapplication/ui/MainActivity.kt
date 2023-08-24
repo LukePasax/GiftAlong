4f4ee078a7168f13bccfe69fa8722f90d834b312
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-08-24 17:50:22 +0200

    Create AppContext to provide context out of activities

--- app/src/main/java/com/giacomosirri/myapplication/ui/MainActivity.kt
+++ app/src/main/java/com/giacomosirri/myapplication/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.giacomosirri.myapplication.ui
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,10 +10,26 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.giacomosirri.myapplication.ui.navigation.NavigationApp
 import com.giacomosirri.myapplication.ui.theme.MyApplicationTheme
+import java.lang.ref.WeakReference
+
+class AppContext private constructor() {
+    companion object {
+        private lateinit var context: WeakReference<Context>
+
+        fun setContext(context: Context) {
+            this.context = WeakReference(context.applicationContext)
+        }
+
+        fun getContext(): Context? {
+            return context.get()
+        }
+    }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppContext.setContext(applicationContext)
         setContent {
             MyApplicationTheme {
                 Surface(
