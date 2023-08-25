bc1d2fe74afdb07b944064f83bbe3a8e3b93906e
Luca Pasini <pasini.luca09@gmail.com>
2023-08-25 15:33:08 +0200

    Add icons to wishlist items

--- app/src/main/java/com/giacomosirri/myapplication/ui/navigation/WishlistScreen.kt
+++ app/src/main/java/com/giacomosirri/myapplication/ui/navigation/WishlistScreen.kt
@@ -1,18 +1,24 @@
 package com.giacomosirri.myapplication.ui.navigation
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
+import com.giacomosirri.myapplication.R
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.giacomosirri.myapplication.ui.AppContext
 import com.giacomosirri.myapplication.ui.theme.Primary
 
 @Composable
@@ -23,23 +29,36 @@ fun WishlistScreen(paddingValues: PaddingValues) {
     }
 }
 
+@SuppressLint("UseCompatLoadingForDrawables")
 @Composable
 fun WishlistItem(name: String, url: String? = null, image: ImageBitmap? = null) {
-    if (image == null) {
-       ListItem(
-           headlineContent = { Text(name) },
-           supportingContent = { Text(url.orEmpty()) },
-           trailingContent = {
-               IconButton(onClick = { /*TODO*/ }) {
-                   Icon(imageVector = Icons.Filled.Delete, contentDescription = "Menu", tint = Primary)
-               }
-           }
-       )
-    } else {
-        ListItem(
-            headlineContent = { Text(name) },
-            supportingContent = { Text(url.orEmpty()) },
-            leadingContent = { Image(bitmap = image, contentDescription = "Image") }
-        )
+    val icon = R.drawable.placeholder_foreground
+    if (image != null) {
+        //TODO: set icon to image
     }
+    ListItem(
+        headlineContent = { Text(name) },
+        supportingContent = { Text(url.orEmpty()) },
+        trailingContent = {
+            Column(horizontalAlignment = Alignment.End) {
+                Row {
+                    Checkbox(checked = false, onCheckedChange = null)
+                    Text(text = "Mark as received")
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Delete,
+                        contentDescription = "Delete",
+                        tint = Primary
+                    )
+                }
+            }
+        },
+        leadingContent = {
+            Image(
+                painterResource(id = icon),
+                contentDescription = "Wishlist item image",
+            )
+        }
+    )
 }
