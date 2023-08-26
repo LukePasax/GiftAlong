912f81b014b8287ef13683cd58e2619336b48e0f
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-08-26 15:28:54 +0200

    Begin implementation of NewEventScreen

--- app/src/main/java/com/giacomosirri/myapplication/ui/navigation/NewEventScreen.kt
+++ app/src/main/java/com/giacomosirri/myapplication/ui/navigation/NewEventScreen.kt
@@ -1,9 +1,82 @@
 package com.giacomosirri.myapplication.ui.navigation
 
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun NewEventScreen(paddingValues: PaddingValues) {
+    Scaffold(modifier = Modifier.padding(paddingValues)) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 10.dp, start = 20.dp, end = 20.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            var eventTitle by remember { mutableStateOf("") }
+            var eventDescription by remember { mutableStateOf("") }
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp),
+                value = eventTitle,
+                onValueChange = { eventTitle = it },
+                label = { Text("Title") },
+            )
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(.25f)
+                    .padding(bottom = 8.dp),
+                value = eventDescription,
+                onValueChange = { eventDescription = it },
+                label = { Text("Description") },
+            )
+            Text(
+                text = "Invite:",
+                modifier = Modifier.padding(bottom = 2.dp),
+                style = MaterialTheme.typography.bodyLarge,
+            )
+            Row(
+                modifier = Modifier
+                    .height(56.dp)
+                    .fillMaxWidth()
+            ) {
+                CheckboxRow("Friends")
+                CheckboxRow("Partner")
+                CheckboxRow("Family")
+            }
+        }
+    }
+}
 
+@Composable
+fun CheckboxRow(text: String) {
+    val (checkedState, onStateChange) = remember { mutableStateOf(true) }
+    Row(
+        modifier = Modifier
+            .defaultMinSize(minWidth = 80.dp)
+            .padding(end = 30.dp)
+            .toggleable(
+                value = checkedState,
+                onValueChange = { onStateChange(!checkedState) },
+                role = Role.Checkbox
+            ),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(
+            checked = checkedState,
+            onCheckedChange = null
+        )
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(start = 8.dp)
+        )
+    }
 }
--- app/src/main/java/com/giacomosirri/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/giacomosirri/myapplication/ui/theme/Color.kt
@@ -15,5 +15,5 @@ val OnBackground = Color(0xFF1C1B1F)
 val OnSurface = Color(0xFF1C1B1F)
 val Error = Color.Red
 val DayCardBackground = Primary
-val EventCardBackground = Color(0xFFD5D4D8)
+val EventCardBackground = Color(0xFFF1F1F3)
 val SpecialEventCardBackground = Color(0xFFF12A1B)
