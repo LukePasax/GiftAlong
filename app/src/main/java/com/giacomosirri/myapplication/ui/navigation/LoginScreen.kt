4ed4f10bc3b31bf5d6014d8345da8c5f211538b8
Luca Pasini <pasini.luca09@gmail.com>
2023-08-28 12:41:25 +0200

    Add buttons to LoginScreen

--- app/src/main/java/com/giacomosirri/myapplication/ui/navigation/LoginScreen.kt
+++ app/src/main/java/com/giacomosirri/myapplication/ui/navigation/LoginScreen.kt
@@ -2,8 +2,11 @@ package com.giacomosirri.myapplication.ui.navigation
 
 import com.giacomosirri.myapplication.ui.theme.Typography
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.sp
 
@@ -13,6 +16,14 @@ fun LoginScreen() {
         Text(text = "GiftAlong", fontSize = 30.sp, style = Typography.headlineLarge)
         OutlinedTextField(value = "Username", onValueChange = "Username"::plus, label = { Text("Username") })
         OutlinedTextField(value = "Password", onValueChange = "Password"::plus, label = { Text("Password") })
-
+        Row {
+            FilledTonalButton(onClick = { /*TODO*/ }) {
+                Text("Login")
+            }
+            FilledTonalButton(onClick = { /*TODO*/ }) {
+                Text(text = "Register")
+            }
+        }
     }
+
 }
