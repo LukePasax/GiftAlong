ec7291c4b8bd3a2171811f9c48b2f51ced517dc6
Luca Pasini <pasini.luca09@gmail.com>
2023-08-28 12:54:03 +0200

    Complete implementation of LoginScreen

--- app/src/main/java/com/giacomosirri/myapplication/ui/navigation/LoginScreen.kt
+++ app/src/main/java/com/giacomosirri/myapplication/ui/navigation/LoginScreen.kt
@@ -1,29 +1,90 @@
 package com.giacomosirri.myapplication.ui.navigation
 
-import com.giacomosirri.myapplication.ui.theme.Typography
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material3.FilledTonalButton
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.giacomosirri.myapplication.ui.theme.Primary
 
 @Composable
-fun LoginScreen() {
-    Column {
-        Text(text = "GiftAlong", fontSize = 30.sp, style = Typography.headlineLarge)
-        OutlinedTextField(value = "Username", onValueChange = "Username"::plus, label = { Text("Username") })
-        OutlinedTextField(value = "Password", onValueChange = "Password"::plus, label = { Text("Password") })
-        Row {
-            FilledTonalButton(onClick = { /*TODO*/ }) {
-                Text("Login")
-            }
-            FilledTonalButton(onClick = { /*TODO*/ }) {
-                Text(text = "Register")
+fun LoginScreen(navController: NavHostController) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        ClickableText(
+            text = AnnotatedString("Sign up here"),
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(20.dp),
+            onClick = { },
+            style = TextStyle(
+                fontSize = 14.sp,
+                fontFamily = FontFamily.Default,
+                textDecoration = TextDecoration.Underline,
+                color = Primary
+            )
+        )
+    }
+    Column(
+        modifier = Modifier.padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        val username = remember { mutableStateOf(TextFieldValue()) }
+        val password = remember { mutableStateOf(TextFieldValue()) }
+
+        Text(text = "GiftAlong", style = TextStyle(fontSize = 40.sp, fontFamily = FontFamily.Cursive))
+
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            label = { Text(text = "Username") },
+            value = username.value,
+            onValueChange = { username.value = it })
+
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            label = { Text(text = "Password") },
+            value = password.value,
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            onValueChange = { password.value = it })
+
+        Spacer(modifier = Modifier.height(20.dp))
+        Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+            Button(
+                onClick = { },
+                shape = RoundedCornerShape(50.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                Text(text = "Login")
             }
         }
-    }
 
+        Spacer(modifier = Modifier.height(20.dp))
+        ClickableText(
+            text = AnnotatedString("Forgot password?"),
+            onClick = { },
+            style = TextStyle(
+                fontSize = 14.sp,
+                fontFamily = FontFamily.Default
+            )
+        )
+    }
 }
