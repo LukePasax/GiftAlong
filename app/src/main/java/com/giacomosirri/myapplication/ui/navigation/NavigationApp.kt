1932bdc9cefec4d3591ef86b352388acae573975
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-08-29 23:54:36 +0200

    Make massive refactoring of NavigationApp

--- app/src/main/java/com/giacomosirri/myapplication/ui/navigation/NavigationApp.kt
+++ app/src/main/java/com/giacomosirri/myapplication/ui/navigation/NavigationApp.kt
@@ -24,13 +24,8 @@ import com.giacomosirri.myapplication.ui.AppContext
 import com.giacomosirri.myapplication.ui.theme.Background
 import com.giacomosirri.myapplication.ui.theme.Primary
 import com.giacomosirri.myapplication.ui.theme.Typography
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
-
-val screensWithSearchBars = mapOf(
-    Pair(AppContext.getContext()?.getString(R.string.home)!!, "Search an event by its title"),
-    Pair(AppContext.getContext()?.getString(R.string.wishlist)!!, "Search an item by its name"),
-    Pair(AppContext.getContext()?.getString(R.string.relationships)!!, "Search any user of this app")
-)
 
 sealed class NavigationScreen(val name: String) {
     object Home: NavigationScreen(AppContext.getContext()?.getString(R.string.home)!!)
@@ -45,107 +40,124 @@ sealed class NavigationScreen(val name: String) {
     object Login: NavigationScreen(AppContext.getContext()?.getString(R.string.login)!!)
 }
 
+class Navigation(
+    val navController: NavHostController,
+    val drawerState: DrawerState,
+    val scope: CoroutineScope,
+    val isSearchBarOpen: MutableState<Boolean>
+)
+
+class Navigator private constructor() {
+    companion object {
+        private lateinit var navigation: Navigation
+        fun setNavigation(navigation: Navigation) { this.navigation = navigation }
+        fun getNavigation(): Navigation = navigation
+    }
+}
+
 class LeadingNavigationIconStrategy(val onBackArrow: () -> Unit, val onMenuIcon: () -> Unit)
 
 @Composable
 fun NavigationApp(navController: NavHostController = rememberNavController(), paddingValues: PaddingValues) {
-    val backStackEntry by navController.currentBackStackEntryAsState()
-    val currentScreen = backStackEntry?.destination?.route ?: NavigationScreen.Login.name
-    val drawerState = rememberDrawerState(DrawerValue.Closed)
-    val scope = rememberCoroutineScope()
-    val strategy = LeadingNavigationIconStrategy({ navController.navigateUp() }, { scope.launch { drawerState.open() } })
-    val isSearchBarOpen = rememberSaveable { mutableStateOf(false) }
-    if (currentScreen != NavigationScreen.Login.name) {
-        ModalNavigationDrawer(
-            drawerState = drawerState,
-            drawerContent = {
-                ModalDrawerSheet(
-                    modifier = Modifier
-                        .fillMaxWidth(.8f)
-                        .fillMaxHeight(),
-                    drawerContainerColor = Background
-                ) {
-                    val items = mapOf(
-                        Pair(
-                            AppContext.getContext()?.getString(R.string.menu_item1),
-                            ImageVector.vectorResource(R.drawable.round_card_giftcard_24)
-                        ),
-                        Pair(
-                            AppContext.getContext()?.getString(R.string.menu_item2),
-                            Icons.Rounded.Favorite
-                        ),
-                        Pair(
-                            AppContext.getContext()?.getString(R.string.menu_item3),
-                            ImageVector.vectorResource(R.drawable.round_group_24)
-                        ),
-                        Pair(
-                            AppContext.getContext()?.getString(R.string.menu_item4),
-                            ImageVector.vectorResource(R.drawable.round_camera_alt_24)
-                        ),
-                        Pair(
-                            AppContext.getContext()?.getString(R.string.menu_item5),
-                            ImageVector.vectorResource(R.drawable.round_query_stats_24)
-                        )
-                    )
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .fillMaxHeight(.08f),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(
-                            text = AppContext.getContext()?.getString(R.string.app_name).toString(),
-                            color = Primary,
-                            style = Typography.headlineLarge
-                        )
-                    }
-                    Spacer(Modifier.height(12.dp))
-                    val selectedItem: MutableState<String?> = remember { mutableStateOf(null) }
-                    items.forEach { item ->
-                        NavigationDrawerItem(
-                            icon = { Icon(item.value, contentDescription = null) },
-                            label = { item.key?.let { Text(it) } },
-                            colors = NavigationDrawerItemDefaults.colors(),
-                            selected = item.key == selectedItem.value,
-                            onClick = {
-                                scope.launch { drawerState.close() }
-                                selectedItem.value = item.key
-                                // Open the search bar only when the user selects the third option from the drawer,
-                                // which navigates to the find new users screen.
-                                isSearchBarOpen.value = selectedItem.value == AppContext.getContext()?.getString(R.string.menu_item3)
-                                navigateFromDrawer(selectedItem.value, navController)
-                            },
-                            modifier = Modifier.padding(bottom = 6.dp, start = 6.dp, end = 6.dp)
-                        )
-                    }
-                    Spacer(Modifier.fillMaxHeight(.85f))
-                    var darkMode by remember { mutableStateOf(false) }
-                    Row(
-                        modifier = Modifier.padding(start = 20.dp, end = 25.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Text("Dark Mode")
-                        Spacer(modifier = Modifier.fillMaxWidth(.8f))
-                        Switch(checked = darkMode, onCheckedChange = { darkMode = it })
-                    }
-                }
-            }
-        ) {
-            Scaffold(
-                topBar = { NavigationAppBar(currentScreen, strategy, isSearchBarOpen) }
-            ) {
-                NavigationGraph(
-                    navController = navController,
-                    paddingValues = paddingValues,
-                )
+    Navigator.setNavigation(Navigation(
+        navController = navController,
+        drawerState = rememberDrawerState(DrawerValue.Closed),
+        scope = rememberCoroutineScope(),
+        isSearchBarOpen = rememberSaveable { mutableStateOf(false) }
+    ))
+    val entryScreenName = navController.currentBackStackEntryAsState().value?.destination?.route ?:
+        NavigationScreen.Login.name
+    if (entryScreenName != NavigationScreen.Login.name) {
+        NavigationDrawer {
+            Scaffold {
+                NavigationGraph(navController, paddingValues)
             }
         }
     } else {
-        NavigationGraph(
-            navController = navController,
-            paddingValues = paddingValues,
-        )
+        NavigationGraph(navController, paddingValues)
     }
+}
+
+@Composable
+private fun NavigationDrawer(content: @Composable () -> Unit) {
+    val navigation = Navigator.getNavigation()
+    ModalNavigationDrawer(
+        drawerState = navigation.drawerState,
+        drawerContent = {
+            ModalDrawerSheet(
+                modifier = Modifier
+                    .fillMaxWidth(.8f)
+                    .fillMaxHeight(),
+                drawerContainerColor = Background
+            ) {
+                val items = mapOf(
+                    Pair(
+                        AppContext.getContext()?.getString(R.string.menu_item1),
+                        ImageVector.vectorResource(R.drawable.round_card_giftcard_24)
+                    ),
+                    Pair(
+                        AppContext.getContext()?.getString(R.string.menu_item2),
+                        Icons.Rounded.Favorite
+                    ),
+                    Pair(
+                        AppContext.getContext()?.getString(R.string.menu_item3),
+                        ImageVector.vectorResource(R.drawable.round_group_24)
+                    ),
+                    Pair(
+                        AppContext.getContext()?.getString(R.string.menu_item4),
+                        ImageVector.vectorResource(R.drawable.round_camera_alt_24)
+                    ),
+                    Pair(
+                        AppContext.getContext()?.getString(R.string.menu_item5),
+                        ImageVector.vectorResource(R.drawable.round_query_stats_24)
+                    )
+                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(.08f),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = AppContext.getContext()?.getString(R.string.app_name).toString(),
+                        color = Primary,
+                        style = Typography.headlineLarge
+                    )
+                }
+                Spacer(Modifier.height(12.dp))
+                val selectedItem: MutableState<String?> = remember { mutableStateOf(null) }
+                items.forEach { item ->
+                    NavigationDrawerItem(
+                        icon = { Icon(item.value, contentDescription = null) },
+                        label = { item.key?.let { Text(it) } },
+                        colors = NavigationDrawerItemDefaults.colors(),
+                        selected = item.key == selectedItem.value,
+                        onClick = {
+                            navigation.scope.launch { navigation.drawerState.close() }
+                            selectedItem.value = item.key
+                            // Open the search bar only when the user selects the third option from the drawer,
+                            // which navigates to the find new users screen.
+                            navigation.isSearchBarOpen.value =
+                                selectedItem.value == AppContext.getContext()?.getString(R.string.menu_item3)
+                            navigateFromDrawer(selectedItem.value, navigation.navController)
+                        },
+                        modifier = Modifier.padding(bottom = 6.dp, start = 6.dp, end = 6.dp)
+                    )
+                }
+                Spacer(Modifier.fillMaxHeight(.85f))
+                var darkMode by remember { mutableStateOf(false) }
+                Row(
+                    modifier = Modifier.padding(start = 20.dp, end = 25.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text("Dark Mode")
+                    Spacer(modifier = Modifier.fillMaxWidth(.8f))
+                    Switch(checked = darkMode, onCheckedChange = { darkMode = it })
+                }
+            }
+        },
+        content = content
+    )
 }
 
 fun navigateFromDrawer(menuItem: String?, navController: NavHostController) {
@@ -231,22 +243,24 @@ fun NavigationGraph(navController: NavHostController, paddingValues: PaddingValu
 @Composable
 fun NavigationAppBar(
     currentScreenName: String,
-    strategy: LeadingNavigationIconStrategy,
-    isSearchBarVisible: MutableState<Boolean>
+    hasSearchBar: Boolean,
+    searchBarPlaceholder: String? = null,
+    isLeadingIconMenu: Boolean = false
 ) {
+    val navigation = Navigator.getNavigation()
     var query by remember { mutableStateOf("") }
     var displayQueriedEvents by remember { mutableStateOf(false) }
-    val isNavigationBarVisible by derivedStateOf { !isSearchBarVisible.value }
-    if (isSearchBarVisible.value) {
+    val isNavigationBarVisible by derivedStateOf { !navigation.isSearchBarOpen.value }
+    if (navigation.isSearchBarOpen.value) {
         SearchBar(
             modifier = Modifier.fillMaxSize(),
             query = query,
             onQueryChange = { query = it },
-            placeholder = { screensWithSearchBars[currentScreenName]?.let { Text(it) } },
+            placeholder = { searchBarPlaceholder!! },
             onSearch = { displayQueriedEvents = true },
             leadingIcon = {
-                IconButton(onClick = { isSearchBarVisible.value = false }) {
-                    Icon(Icons.Rounded.ArrowBack, "Close search bar")
+                IconButton(onClick = { navigation.isSearchBarOpen.value = false }) {
+                    Icon(Icons.Rounded.ArrowBack, "Close icon")
                 }
             },
             trailingIcon = {
@@ -254,7 +268,7 @@ fun NavigationAppBar(
                     query = ""
                     displayQueriedEvents = false
                 }) {
-                    Icon(Icons.Rounded.Clear, "Clear searched text")
+                    Icon(Icons.Rounded.Clear, "Clear icon")
                 }
             },
             active = true,
@@ -272,41 +286,31 @@ fun NavigationAppBar(
         }
     }
     if (isNavigationBarVisible) {
+        val navIconStrategy = LeadingNavigationIconStrategy(
+            onBackArrow = { navigation.navController.navigateUp() },
+            onMenuIcon = { navigation.scope.launch { navigation.drawerState.open() } }
+        )
         CenterAlignedTopAppBar(
             actions = {
                 // Only a handful of screens should have the search icon in the top app bar.
-                if (screensWithSearchBars.contains(currentScreenName)) {
-                    IconButton(onClick = { isSearchBarVisible.value = true }) {
-                        Icon(
-                            imageVector = Icons.Filled.Search,
-                            contentDescription = "Search event"
-                        )
+                if (hasSearchBar) {
+                    IconButton(onClick = { navigation.isSearchBarOpen.value = true }) {
+                        Icon(Icons.Filled.Search, "Search icon")
                     }
                 }
             },
             title = {
-                Text(
-                    text = currentScreenName,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis
-                )
+                Text(text = currentScreenName, maxLines = 1, overflow = TextOverflow.Ellipsis)
             },
             navigationIcon = {
-                when (currentScreenName) {
-                    AppContext.getContext()?.getString(R.string.home) ->
-                        IconButton(onClick = strategy.onMenuIcon) {
-                            Icon(
-                                imageVector = Icons.Filled.Menu,
-                                contentDescription = "Main menu"
-                            )
-                        }
-                    else ->
-                        IconButton(onClick = strategy.onBackArrow) {
-                            Icon(
-                                imageVector = Icons.Filled.ArrowBack,
-                                contentDescription = "Go back"
-                            )
-                        }
+                if (isLeadingIconMenu) {
+                    IconButton(onClick = navIconStrategy.onMenuIcon) {
+                        Icon(Icons.Filled.Menu, "Main menu icon")
+                    }
+                } else {
+                    IconButton(onClick = navIconStrategy.onBackArrow) {
+                        Icon(Icons.Filled.ArrowBack, "Go back icon")
+                    }
                 }
             },
             colors = TopAppBarDefaults.centerAlignedTopAppBarColors(containerColor = Primary)
