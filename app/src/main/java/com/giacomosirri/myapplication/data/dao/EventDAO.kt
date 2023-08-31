5cd6bb6ddc451c73c1d7a60d8c3d3f3961678489
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-08-31 11:30:20 +0200

    Add queries to EventRepository

--- app/src/main/java/com/giacomosirri/myapplication/data/dao/EventDAO.kt
+++ app/src/main/java/com/giacomosirri/myapplication/data/dao/EventDAO.kt
@@ -1,7 +1,20 @@
 package com.giacomosirri.myapplication.data.dao
 
-import androidx.room.Dao
+import androidx.room.*
+import com.giacomosirri.myapplication.data.entity.Event
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface EventDAO {
+    @Query("")
+    fun getEvents(): Flow<List<Event>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(event: Event)
+
+    @Delete
+    suspend fun delete(event: Event)
+
+    @Query("")
+    suspend fun deleteAll()
 }
--- app/src/main/java/com/giacomosirri/myapplication/repository/EventRepository.kt
+++ app/src/main/java/com/giacomosirri/myapplication/repository/EventRepository.kt
@@ -1,6 +1,25 @@
 package com.giacomosirri.myapplication.repository
 
+import androidx.annotation.WorkerThread
 import com.giacomosirri.myapplication.data.dao.EventDAO
+import com.giacomosirri.myapplication.data.entity.Event
+import kotlinx.coroutines.flow.Flow
 
 class EventRepository(private val eventDAO: EventDAO) {
+    val allItems: Flow<List<Event>> = eventDAO.getEvents()
+
+    @WorkerThread
+    suspend fun insertEvent(event: Event) {
+        eventDAO.insert(event)
+    }
+
+    @WorkerThread
+    suspend fun deleteEvent(event: Event) {
+        eventDAO.delete(event)
+    }
+
+    @WorkerThread
+    suspend fun deleteAllItems() {
+        eventDAO.deleteAll()
+    }
 }
