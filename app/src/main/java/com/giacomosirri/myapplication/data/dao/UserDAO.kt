60c2df5830cebee4691d036d8e61d58ce12a5b15
Luca Pasini <pasini.luca09@gmail.com>
2023-08-31 12:22:30 +0200

    Add User Queries

--- app/src/main/java/com/giacomosirri/myapplication/data/dao/UserDAO.kt
+++ app/src/main/java/com/giacomosirri/myapplication/data/dao/UserDAO.kt
@@ -1,7 +1,16 @@
 package com.giacomosirri.myapplication.data.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.giacomosirri.myapplication.data.entity.User
 
 @Dao
 interface UserDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(user: User)
+
+    @Query("SELECT * FROM users WHERE username = :username AND password = :password")
+    suspend fun getUser(username: String, password: String): User?
 }
