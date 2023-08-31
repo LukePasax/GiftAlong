eb503794038229794f96003d4f2d1a995a16444f
Giacomo Sirri <sirrigiacomo9@gmail.com>
2023-08-31 16:17:56 +0200

    Move photo selector to CommonComposables

--- app/src/main/java/com/giacomosirri/myapplication/ui/navigation/CommonComposables.kt
+++ app/src/main/java/com/giacomosirri/myapplication/ui/navigation/CommonComposables.kt
@@ -3,16 +3,29 @@ package com.giacomosirri.myapplication.ui.navigation
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.DateRange
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.giacomosirri.myapplication.R
 import com.giacomosirri.myapplication.ui.theme.Typography
+import java.text.DateFormat
+import java.util.*
 
 @Composable
 fun DialogCard(
@@ -112,4 +125,89 @@ fun DialogTitle(
         textAlign = TextAlign.Center,
         style = Typography.headlineLarge
     )
+}
+
+@Composable
+fun PhotoSelector() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painterResource(id = R.drawable.placeholder),
+            contentDescription = "Item image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .requiredSize(width = 165.dp, height = 140.dp)
+                .clip(RoundedCornerShape(5.dp))
+        )
+        FilledTonalButton(
+            modifier = Modifier.padding(start = 15.dp),
+            onClick = { /* TODO select an image from the gallery */ }
+        ) {
+            Icon(
+                modifier = Modifier.padding(end = 5.dp),
+                imageVector = ImageVector.vectorResource(R.drawable.round_camera_alt_24),
+                contentDescription = null
+            )
+            Text("Select a photo")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DateDialog(
+    paddingValues: PaddingValues,
+    buttonText: String
+) {
+    // Date dialog
+    val isDateDialogOpen = remember { mutableStateOf(false) }
+    val datePickerState = rememberDatePickerState(initialSelectedDateMillis = Date().time)
+    val confirmEnabled = derivedStateOf { datePickerState.selectedDateMillis != null }
+    if (isDateDialogOpen.value) {
+        DatePickerDialog(
+            onDismissRequest = {
+                // Dismiss the dialog when the user clicks outside the dialog or on the back
+                // button. If you want to disable that functionality, simply use an empty
+                // onDismissRequest.
+                isDateDialogOpen.value = false
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = { isDateDialogOpen.value = false },
+                    enabled = confirmEnabled.value
+                ) {
+                    Text("OK")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { isDateDialogOpen.value = false }) {
+                    Text("Cancel")
+                }
+            }
+        ) {
+            DatePicker(state = datePickerState)
+        }
+    }
+    // Date picker
+    Row(
+        modifier = Modifier.padding(paddingValues),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            modifier = Modifier.padding(end = 8.dp),
+            text = DateFormat.getDateInstance().format(Date(datePickerState.selectedDateMillis!!))
+        )
+        FilledTonalButton(onClick = { isDateDialogOpen.value = true }) {
+            Icon(
+                modifier = Modifier.padding(end = 5.dp),
+                imageVector = Icons.Rounded.DateRange,
+                contentDescription = null
+            )
+            Text(buttonText)
+        }
+    }
 }
